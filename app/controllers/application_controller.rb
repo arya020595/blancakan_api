17acf66b8b3c9e8a42247b14cd9bc62e95f944f9
arya020595 <aryarifqipratama@gmail.com>
2025-02-10 22:16:38 +0700

    display error forbidden if have no access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { error: exception.message }, status: :forbidden
+  end
 end
