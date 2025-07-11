c8ab92dc80952023ec9ed2dda133be7d6ca4575b
arya020595 <aryarifqipratama@gmail.com>
2025-07-11 09:00:38 +0700

    Add event types seed file to the seeding process

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 load Rails.root.join('db/seeds/roles_and_permissions.rb')
 load Rails.root.join('db/seeds/users.rb')
 load Rails.root.join('db/seeds/categories.rb')
+load Rails.root.join('db/seeds/event_types.rb')
 load Rails.root.join('db/seeds/events.rb')
 
 puts 'Seeding process completed!'
