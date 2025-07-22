9c132b87b1cf477ec096524e03643782a613dc7a
arya020595 <aryarifqipratama@gmail.com>
2025-07-22 21:57:15 +0700

    Add organizers seed file to the seeding process

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # Load seed files
 load Rails.root.join('db/seeds/roles_and_permissions.rb')
 load Rails.root.join('db/seeds/users.rb')
+load Rails.root.join('db/seeds/organizers.rb')
 load Rails.root.join('db/seeds/categories.rb')
 load Rails.root.join('db/seeds/event_types.rb')
 load Rails.root.join('db/seeds/events.rb')
