f609046fcda4005a220fbe3fe1ebd071936ed2e3
arya020595 <aryarifqipratama@gmail.com>
2025-07-23 19:54:14 +0700

    Add organizer service registration to the container

--- config/initializers/container.rb
+++ config/initializers/container.rb
@@ -6,6 +6,7 @@ class Container
   register('v1.role_service') { V1::RoleService.new }
   register('v1.permission_service') { V1::PermissionService.new }
   register('v1.user_service') { V1::UserService.new }
+  register('v1.organizer_service') { V1::OrganizerService.new }
   register('v1.event_service') { V1::EventService.new }
   register('v1.category_service') { V1::CategoryService.new }
   register('v1.event_type_service') { V1::EventTypeService.new }
